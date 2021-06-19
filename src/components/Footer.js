eaa0fb6710bb2186952264957e766d2d29d424cd
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-06-19 12:55:41 +0800

    добавляет год в футере

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import cx from 'classnames';
 
+const date = new Date();
+
 function Footer({mix}) {
   return (
     <footer className={cx(mix, 'footer')}>
-      <p className="footer__copyright">© 2020 Mesto Russia</p>
+      <p className="footer__copyright">© {date.getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
